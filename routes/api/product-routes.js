b7119086d4af08a867cadc083d1a35dcf9383129
void2412 <void2412@gmail.com>
2022-06-07 16:32:02 +0930

    finished routes for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -109,8 +109,17 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+	const data = await Product.destroy({
+		where: {id:req.params.id}
+	})
+	res.status(200).json(data);
+  }
+  catch(err){
+	  res.status(400).json(err)
+  }
 });
 
 module.exports = router;
