ec2337d5079faadf0c6ca068cddb4562346d03d4
void2412 <void2412@gmail.com>
2022-06-07 15:38:06 +0930

    finished single item get category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,13 +18,24 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+	  const categoryById = await Category.findByPk(req.params.id, {
+		  include: [{model: Product}]
+	  })
+
+	  res.status(200).json(categoryById)
+  }
+  catch(err){
+	  res.status(200).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  
 });
 
 router.put('/:id', (req, res) => {
