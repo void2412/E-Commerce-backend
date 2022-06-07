ab8ab17504069d679147d5e67ba70d4a5150c15e
void2412 <void2412@gmail.com>
2022-06-07 15:50:52 +0930

    added delete for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -61,8 +61,19 @@ router.put('/:id', async (req, res) => {
 	}
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try{
+	  const updatedData = await Category.destroy({
+		  where: {
+			  id: req.params.id
+		  }
+	  })
+	  res.status(200).json(updatedData)
+  }
+  catch(err){
+	  res.status(500).json(err)
+  }
 });
 
 module.exports = router;
